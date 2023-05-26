a38635669297c2bf404dc6530fd45594f5327772
iTzKittyMoon <iamkittymoon@gmail.com>
2023-05-25 23:58:00 -0300

    Set text in .kt file instead of .xml

--- app/src/main/java/com/example/orgs/MainActivity.kt
+++ app/src/main/java/com/example/orgs/MainActivity.kt
@@ -11,5 +11,14 @@ class MainActivity: Activity() {
     override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val name = findViewById<TextView>(R.id.name)
+        name.text = "Cesta de frutas"
+        val description = findViewById<TextView>(R.id.description)
+        description.text = "Laranja, manga e maçã"
+        val value = findViewById<TextView>(R.id.value)
+        value.text = "19,99"
+
+
     }
 }
+
